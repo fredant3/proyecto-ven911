34a955c782abd89631f11488005d4d54f80eac36
Freddy <144859937+fredant3@users.noreply.github.com>
2023-09-21 23:05:27 -0400

    primeros pasos

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 from flask import Flask
+from psycopg2 import connect
 
 app = Flask(__name__)
+
+
+host = 'localhost'
+port = 5432
+dbname = 'usersdb'
+user = 'postgres'
+password = 'postgres'
+
+
+def get_connection():
+    conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
+    return conn
+
+
+@app.get ('/')
+def home():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    result = cur.execute("SELECT 1 + 1")
+    print(result)
+
+
+    return 'Hola mundo...!'
 
 if __name__ == '__main__':
     app.run(debug=True)
