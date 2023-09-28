0e7329f20d356d56f599985e39ce612c4983dbf9
Freddy <144859937+fredant3@users.noreply.github.com>
2023-09-27 23:17:26 -0400

    avance

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask
-from psycopg2 import connect
+from flask import Flask, request, jsonify
+from psycopg2 import connect, extras
+from cryptography.fernet import Fernet
 
 app = Flask(__name__)
+key = Fernet.generate_key()
 
 
-host = 'localhost'
+host = "localhost"
 port = 5432
-dbname = 'usersdb'
-user = 'postgres'
-password = 'postgres'
+dbname = "usersdb"
+user = "postgres"
+password = "postgres"
 
 
 def get_connection():
@@ -16,16 +18,46 @@ def get_connection():
     return conn
 
 
-@app.get ('/')
-def home():
+@app.get("/api/users")
+def get_users():
+    return "getting users"
+
+
+@app.post("/api/users")
+def create_users():
+    new_user = request.get_json()
+    username = new_user["username"]
+    password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
+
     conn = get_connection()
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
-    result = cur.execute("SELECT 1 + 1")
-    print(result)
+    cur.execute("INSERT INTO users (username, password) VALUES (%s, %s) RETURNING *",
+                (username, password))
+    new_created_user = cur.fetchone()
+    print(new_created_user)
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+
+    return jsonify(new_created_user)
 
 
-    return 'Hola mundo...!'
+@app.delete("/api/users")
+def delete_users():
+    return "deleting users"
 
-if __name__ == '__main__':
+
+@app.put("/api/users")
+def update_users():
+    return "updating users"
+
+
+@app.get("/api/users")
+def get_uses():
+    return "getting user"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
